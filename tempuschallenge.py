0400e702a6fe059553d865800fc9e1b6271bb1d6
User <user@Administrators-Mac.local>
2017-10-25 17:25:51 -0500

    Final version of challenge code, with changes to output file (now CSV) and added ANNOVAR-annotated VCF

--- tempuschallenge.py
+++ tempuschallenge.py
@@ -1,27 +1,73 @@
 import requests
+import os
+from subprocess import call
 
 def tempuschallenge():
-	data_old = open('Challenge_data.vcf','r')
-	data_new = open('Annotated_data.vcf','w')
+	# This challenge takes in Challenge_data.vcf and returns a CSV file containing annotated information about the genome.
+	# Output file is Annotated_data.csv.
+	# Information in CSV in order:
+	# - Chromosome
+	# - Index
+	# - Reference sequence
+	# - Variant sequence
+	# - Type of mutation
+	# - Depth of sequence coverage
+	# - Proportion of reads supporting variant allele
+	# - Proportion of reads supporting reference allele
+	# - ExAC allele frequency of variant
+
+	# Run ANNOVAR on input data to get exact allele functions
+	call(['perl', '../annovar/table_annovar.pl', 'Challenge_data.vcf', '../annovar/humandb/', '-buildver', 'hg19', '-out', 'annotated', '-remove', '-protocol', 'refGene,cytoBand,exac03', '-operation', 'g,r,f', '-nastring','.','-vcfinput'])
+	
+	# Open files for processing 
+	data_old = open('annotated.hg19_multianno.vcf','r')
+	data_new = open('Annotated_data.csv','w')
 	data = data_old.read().split('\n')[:-1]
+
+	# columns for new data table
+	data_new.write("chrom,index,reference,variant,type,depth,percentSupportingVariant,percentSupportingReference,ExACAlleleFreq\n")
 	for line in data:
-		if line[0] != '#':
+		if line[0] != '#': # ignore input flags
 			# split line into each possible element
 			lineData = line.split('\t')
 
-			# get type of variant
-			typeOfVariant = lineData[-4].split('=')[-1]
-
-			#parse flags of variant to get additional metadata
+			#parse ending flags of variant to get additional metadata
 			flags = lineData[-2].split(':')
 			depth = flags[2] # sequencing depth
 
 			#determine most deleterious variant
 			potentialVars = lineData[4].split(',')
 			var = min(potentialVars,key=len)
-			varIndex = potentialVars.index(var) #get index of deleterious variant for future 
+			varIndex = potentialVars.index(var) #get index of deleterious variant for future reference
 
-			# get percentages of reference/alternate alleles
+			# get type of variant from ANNOVAR annotation
+			variantFlags = lineData[-4].split(';')
+			checkIndex = 0
+			needExactFunction = False
+			for f in variantFlags:
+				if "Func.refGene" in f:
+					if checkIndex == varIndex: # want to make sure correct variant info is returned when VCF indicates more than one possible variant
+						if "ExonicFunc.refGene" in f and needExactFunction: # if mutation is exonic, need exact effect of mutation on coding region
+							typeOfVariant = f.split('=')[1]
+							if typeOfVariant == "stopgain": # clarifying ANNOVAR teminology
+								typeOfVariant = "nonsense"
+							elif typeOfVariant == "stoploss":
+								typeOfVariant = "stop_codon_loss"
+							elif typeOfVariant == "nonsynonymous_SNV":
+								typeOfVariant = "missense"
+							elif typeOfVariant == "synonymous_SNV":
+								typeOfVariant = "silent"
+							break
+						else: 
+							typeOfVariant = f.split('=')[1]
+							if typeOfVariant == "exonic":
+								needExactFunction = True
+							else: 
+								break
+					else:
+						checkIndex += 1
+					
+			# get percentages of reference/alternate alleles compared to read depth
 			supportingVar = int(flags[4]);
 			supportingRef = int(flags[6].split(',')[varIndex]);
 			percentSupportingVar = float(supportingVar)/int(depth)
@@ -35,7 +81,8 @@ def tempuschallenge():
 
 			# get relevant ExAC allele frequency info
 			key = '"allele_freq":'
-			response = requests.get('http://exac.hms.harvard.edu/rest/variant/'+'-'.join(varMetadata))
+			response = requests.get('http://exac.hms.harvard.edu/rest/variant/'+'-'.join(varMetadata)) # query ExAC server for info
+			print 'http://exac.hms.harvard.edu/rest/variant/'+'-'.join(varMetadata)
 			response = response.text.encode('utf-8').split()
 			try:
 				queryIndex = response.index(key)
@@ -44,15 +91,12 @@ def tempuschallenge():
 			except ValueError: #variant not found in ExAC, or no info found for allele frequency
 				allele_freq = 0.0 #we assume allele hasn't been found yet
 
-			newData = [typeOfVariant,depth,str(supportingVar),str(percentSupportingVar),str(percentSupportingRef),str(allele_freq)]
+			# combine all data in line and write to file
+			newData = varMetadata + [typeOfVariant,depth,str(supportingVar),str(percentSupportingVar),str(percentSupportingRef),str(allele_freq)]
+			line = ','.join(newData)
+			data_new.write(line + '\n')
+	data_new.close()
 
-			line += '\t' + ':'.join(newData)
-			data_new.write(line + '\n')
-		else:
-			if line[1] != '#':
-				line += '\t' + 'alleleinfo'
-			data_new.write(line + '\n')
-			
 if __name__ == '__main__':
-	output_challenge()
+	tempuschallenge()
 
